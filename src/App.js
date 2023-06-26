67ef6c07d3795b3c700cad3c9ee3c884257eb0b5
Gabe Grant <gabe.grant@proton.me>
2023-06-25 21:11:27 -0400

    trying to get these radio buttons to uncheck

--- src/App.js
+++ src/App.js
@@ -1,37 +1,62 @@
 import ons from 'onsenui';
-import { Page, Button } from 'react-onsenui';
-// import * as Ons from 'react-onsenui';
+import * as Ons from 'react-onsenui';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 import { useState } from 'react';
 
 function App() {
-    const [text, setText] = useState('');
+  const [text, setText] = useState('');
+  const [radio, setRadio] = useState();
 
-    const handleClick = () => {
-        ons.notification.alert('Hello world!');
-    }
-
-    const handleChange = (event) => {
-        setText(event.target.value);
-    };
-
+  const renderToolbar = () => {
     return (
-        <div>
-            <Page>
-                <label>Input Text:</label>
-                <div>
-                    <input
-                        value={text}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <Button onClick={handleClick}>Tap Me</Button>
-                </div>
-            </Page>
-        </div>
+      <Ons.Toolbar>
+        <div className='center'>My Simple App</div>
+      </Ons.Toolbar>
     );
+  }
+
+  const handleClick = () => {
+    ons.notification.alert('Hello world!');
+  }
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleRadio = (event) => {
+    setRadio(event.target.checked);
+  };
+
+  return (
+    <Ons.Page renderToolbar={renderToolbar}>
+      <div onChange={handleRadio}>
+        <Ons.Radio
+          modifier='material' 
+        />
+        <Ons.Radio
+          checked={radio}
+          modifier='material'
+        />
+        <Ons.Radio
+          checked={radio}
+          modifier='material' 
+        />
+      </div>
+      <label>Input Text:</label>
+      <div>
+        <Ons.Input
+          modifier='material'
+          placeholder='type here'
+          value={text}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <Ons.Button modifier='material' onClick={handleClick}>Tap Me</Ons.Button>
+      </div>
+    </Ons.Page>
+  );
 }
 
 export default App;
